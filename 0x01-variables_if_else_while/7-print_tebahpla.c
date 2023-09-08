b09721ebc7f32ca24b91b39356aaa8dfad6fe02b
Kidus Gebru <kidusgebruhagos@gmail.com>
2023-09-08 19:48:32 +0000

    Complete Task 7 of 213

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 122; n >= 97; n--)
+	{
+		putchar(n);
+	}
+	putchar(10);
+	return (0);
+}
