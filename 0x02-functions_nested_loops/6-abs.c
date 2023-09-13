fd4f1565a8314e749da078a20fc45b83d6b0ae57
Kidus Gebru <kidusgebruhagos@gmail.com>
2023-09-13 13:38:57 -0700

    Complete Task 6 of 214

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+/**
+ * _abs - Returns absolute value of n
+ * @n: Input number
+ *
+ * Return: |n|
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-1 * n);
+	}
+}
+
