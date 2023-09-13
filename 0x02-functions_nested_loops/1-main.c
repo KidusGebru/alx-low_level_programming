4a28c3b2dad56a3ce1fc2b7db775429fc795eb8c
Kidus Gebru <kidusgebruhagos@gmail.com>
2023-09-13 15:46:48 -0700

    Complete Task 8 of 214

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -3,31 +3,5 @@
 
 int main(void)
 {
-	int r;
-	r = 2;
-	_putchar('0' + r);
-	_putchar(32);
-	print_last_digit(r);
-	_putchar(10);
-	r = -1024;
-	_putchar('0' + r);
-	_putchar(32);
-	print_last_digit(r);
-	_putchar(10);
-	r = INT_MAX;
-	_putchar('0' + r);
-	_putchar(32);
-	print_last_digit(r);
-	_putchar(10);
-	r = INT_MIN;
-	_putchar('0' + r);
-	_putchar(32);
-	print_last_digit(r);
-	_putchar(10);
-	r = 1;
-	_putchar('0' + r);
-	_putchar(32);
-	print_last_digit(r);
-	_putchar(10);
-
+	jack_bauer();
 }
