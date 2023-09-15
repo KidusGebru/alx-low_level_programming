6f19c4c902344e2d1ced8f402749f448311a80f7
Kidus Gebru <kidusgebruhagos@gmail.com>
2023-09-15 11:03:25 -0700

    Complete Tasl 2 of 215

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - Returns the product of input operands
+ *@a: First operand
+ *@b: Second operand
+ *
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
