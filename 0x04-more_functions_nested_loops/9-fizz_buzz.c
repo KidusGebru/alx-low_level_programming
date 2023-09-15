3c7ed89f1bc77b714e82fc94e573bb0f7c4b94c1
Kidus Gebru <kidusgebruhagos@gmail.com>
2023-09-15 13:11:57 -0700

    Complete Task 9 of 215

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Program prints numbers 1 to 100 with words fizz buzz here and there
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+			printf("Fizz ");
+		else if (i % 3 != 0 && i % 5 == 0)
+			printf("Buzz ");
+		else if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
+
