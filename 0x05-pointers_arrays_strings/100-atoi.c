5f58143ff5f4b450b119c3c4c8714cd4b2f18c5b
Kidus Gebru <kidusgebruhagos@gmail.com>
2023-09-18 16:29:04 -0700

    Complete Task 10 of 216;

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * _atoi - Converts string to an integer
+ * @s: the src string
+ *
+ * Return: Void
+ */
+
+int _atoi(char *s)
+{
+	int num, k, j = 0, res = 0, mul = 1, sign = 1, i = 0, discard = 0;
+	char ch;
+
+	while (*(s + i) != '\0')
+		i++;
+
+	for (j = --i; j >= 0; j--)
+	{
+		ch = *(s + j);
+		if ((ch >= '0' && ch <= '9') || ch == '-')
+		{
+			if (ch == '-')
+				sign *= -1;
+			else
+			{
+				num = 0;
+				if (discard == 1)
+				{
+					sign = mul = 1;
+					res = discard = 0;
+				}
+
+				for (k = '0'; k <= '9'; k++)
+				{
+					if (k == ch)
+					{
+						res += mul * num;
+						mul *= 10;
+						break;
+					}
+					num++;
+				}
+			}
+		}
+		else if (ch != '+' && ch != ' ')
+		{
+			discard = 1;
+		}
+	}
+	return (sign * res);
+}
--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -3,11 +3,23 @@
 
 int main(void)
 {
-	char s1[98];
-	char *ptr;
+	int nb;
 	
-	ptr = _strcpy(s1, "First, solve the problem. Then, write the code\n");
-	printf("%s", s1);
-	printf("%s", ptr);
+	nb = _atoi("98");
+	printf("%d\n", nb);
+	nb = _atoi("-402");
+	printf("%d\n", nb);
+	nb = _atoi("              ------++++++-----+++++--98");
+	printf("%d\n", nb);
+	nb = _atoi("214748364");
+	printf("%d\n", nb);
+	nb = _atoi("0");
+	printf("%d\n", nb);
+	nb = _atoi("Suite 402");
+	printf("%d\n", nb);
+	nb = _atoi("          +       +    -     -98 Battery Stree; Ca 94");
+	printf("%d\n", nb);
+	nb = _atoi("---++++ -++ Suite - te -      402 #cisfun :)");
+	printf("%d\n", nb);
 	return(0);
 }
