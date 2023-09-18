b586233a6e9c3b1d31f4f70abf970e45889f46bb
Kidus Gebru <kidusgebruhagos@gmail.com>
2023-09-18 09:37:30 -0700

    Complete Task 4 of 216

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse into a stdout
+ * @s: the string
+ *
+ * Return: Void
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+		i++;
+	for (; i >= 0; i--)
+		_putchar(*(s + i));
+	_putchar('\n');
+}
