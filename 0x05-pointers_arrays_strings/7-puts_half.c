cbba5b74e80e2a4c300e6cb9385176d8ce6dc11d
Kidus Gebru <kidusgebruhagos@gmail.com>
2023-09-18 13:21:53 -0700

    Complete Task 7 of 216

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints half of a given string
+ * @str: the string
+ *
+ * Return: Void
+ */
+
+void puts_half(char *str)
+{
+	int i, len = 0;
+
+	while (*(str + len) != '\0')
+	{
+		len++;
+	}
+	for (i = (len + 1) / 2; i <= len; i++)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -5,8 +5,8 @@ int main(void)
 {
 	char *str;
 	
-	str = "0123456789";
-	puts2(str);
+	str = "01234567";
+	puts_half(str);
 
 	return(0);
 }
