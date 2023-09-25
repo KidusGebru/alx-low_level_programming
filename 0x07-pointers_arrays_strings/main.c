34b788c43b17c6e02c0439c8c0c38ea838a1d890
Kidus Gebru <kidusgebruhagos@gmail.com>
2023-09-25 14:25:34 -0700

    Complete Task 6 of 218

--- 0x07-pointers_arrays_strings/main.c
+++ 0x07-pointers_arrays_strings/main.c
@@ -8,12 +8,17 @@
  *     */
 int main(void)
 {
-	    char *s = "hello, world is not";
-	        char *f = "world";
-		    char *t;
-
-		        t = _strstr(s, f);
-			    printf("%s\n", t);
-			        return (0);
+	    char board[8][8] = {
+		            {'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
+			            {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+				            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+					            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+						            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+							            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+								            {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+									            {'R', 'K', 'B', 'Q', 'K', 'B', 'K', 'R'},
+										        };
+	        print_chessboard(board);
+		    return (0);
 }
 
