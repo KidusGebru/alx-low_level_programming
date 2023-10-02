60f4ea99eb5282ef8f7488603f79a5697970d8f4
Kidus Gebru <kidusgebruhagos@gmail.com>
2023-10-02 12:32:21 -0700

    Complete Task 3 of 221

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints multiplication result of two numbers passed as arguments
+ * @argc: Argument counter
+ * @argv: Arguments values
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[1]));
+	}
+
+	return (0);
+}
