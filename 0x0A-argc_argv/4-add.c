eb370776a64aba7cecd77f18e61940ae86d0a83c
Kidus Gebru <kidusgebruhagos@gmail.com>
2023-10-02 13:24:45 -0700

    Iteration 1 of Task 4 of 221

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,7 +12,7 @@
 
 int main(int argc, char *argv[])
 {
-	int i, res = 0;
+	int i, j = 0, res = 0;
 
 	if (argc < 2)
 	{
@@ -23,14 +23,23 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (atoi(argv[i]) > 0 || (*(argv[i]) == '0' && *(argv[i] + 1) == 0))
+			if (atoi(argv[i]) > 0)
 			{
 				res += atoi(argv[i]);
 			}
 			else
 			{
-				printf("Error\n");
-				return (1);
+				while (*(argv[i] + j) != 0)
+				{
+					if (!(*(argv[i] + j) >= '0' && *(argv[i] + j) <= '9'))
+					{
+						printf("Error\n");
+						return (1);
+					}
+					j++;
+				}
+				j = 0;
+				res += atoi(argv[i]);
 			}
 		}
 
