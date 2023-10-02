6badd26f0d713ff4c47250d6a99e2d1caafc8345
Kidus Gebru <kidusgebruhagos@gmail.com>
2023-10-02 11:01:51 -0700

    Complete Task 0 of 220

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _memset - dummy function
+ * @s: arg 1
+ * @b: arg 2
+ * @n: arg 3
+ *
+ * Return: Always void
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (s);
+}
---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _memcpy - dummy function
+ * @dest: arg 1
+ * @src: arg 2
+ * @n: arg 3
+ *
+ * Return: Always void
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	return (dest);
+}
