9396053037f2203015ba9d1666551d667cfce533
Kidus Gebru <kidusgebruhagos@gmail.com>
2023-10-03 12:45:55 -0700

    Complete Task 1 of 222

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - Creates copy of a string argument in memory
+ * @str; Argument string
+ *
+ * Return: Pointer to copy in memory
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int i = 0, j = 0;
+	char *dup;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[i] != 0)
+		i++;
+
+	dup = malloc(++i * sizeof(dup));
+	if (dup == NULL)
+		return (NULL);
+	
+	while (j < i)
+	{
+		dup[j] = str[j];
+		j++;
+	}
+	return (dup);
+}
