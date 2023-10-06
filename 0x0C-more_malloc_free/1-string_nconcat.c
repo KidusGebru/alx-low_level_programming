4946c0da04f64130c8cbb1fd30a78831164d68ad
Kidus Gebru <kidusgebruhagos@gmail.com>
2023-10-06 13:09:33 -0700

    Iteration 1 of Task 1 of 223

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,6 +15,11 @@ char  *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *res;
 	unsigned int size, j = 0, i = 0;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	while (s1[i] != '\0')
 		i++;
 	size = i + n + 1;
