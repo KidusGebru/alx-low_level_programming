e1cf513f97e44e6c5577aa1fc3fe034e973bb132
Kidus Gebru <kidusgebruhagos@gmail.com>
2023-10-06 13:35:25 -0700

    Iteration 2 of Task 2 of 223

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,16 +13,17 @@ void  *_calloc(unsigned int nmemb, unsigned int size)
 	char *arr;
 	unsigned int i;
 
-	if (nmemb == 0 || size == 0)
-		return (NULL);
+	if (nmemb > 0 && size > 0)
+	{
+		arr = malloc(nmemb * size);
+		if (arr == NULL)
+			return (NULL);
 
-	arr = malloc(nmemb * size);
-	if (arr == NULL)
-		return (NULL);
+		for (i = 0; i < nmemb * size; i++)
+			arr[i] = 0;
 
-	for (i = 0; i < nmemb * size; i++)
-		arr[i] = 0;
-
-	arr[nmemb] = 0;
-	return (arr);
+		arr[nmemb] = 0;
+		return (arr);
+	}
+	return (NULL);
 }
