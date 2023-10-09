51a73b2e6697f74a7f1904201793a42049c411eb
Kidus Gebru <kidusgebruhagos@gmail.com>
2023-10-09 12:05:54 -0700

    Complete Task 0 of 225

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef KIDUS_225_TASK0
+#define KIDUS_225_TASK0
+
+/**
+ * struct dog - Structure for storing dog information
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog ownder name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
