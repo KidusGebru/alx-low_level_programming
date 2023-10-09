054fce2975c01f1a6553ec557c43d9d618d444a0
Kidus Gebru <kidusgebruhagos@gmail.com>
2023-10-09 12:54:07 -0700

    Iteration 2 of Task 1 of 225

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "dog.h"
 /**
  * init_dog - Initializes a variables of type struct dog
@@ -11,6 +12,12 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+	{
+		d = malloc(sizeof(struct dog));
+		if (d == NULL)
+			exit(1);
+	}
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
