3e6a3bc2e02cc629a4852023e0d98bfd502b55d2
Kidus Gebru <kidusgebruhagos@gmail.com>
2023-10-09 13:50:04 -0700

    Complete Task 3 of 225

--- 0x0E-structures_typedef/main.c
+++ 0x0E-structures_typedef/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "dog.h"
+
 /**
  *  * main - check the code
  *   *
@@ -7,9 +8,11 @@
  *     */
 int main(void)
 {
-	    struct dog my_dog;
+	    dog_t my_dog;
 
-	        my_dog.name = NULL;
-			    print_dog(&my_dog);
+	        my_dog.name = "Poppy";
+		    my_dog.age = 3.5;
+		        my_dog.owner = "Bob";
+			    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
 			        return (0);
 }
