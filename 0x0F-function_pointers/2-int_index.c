27d610b4ac8da8037b212bb9b299446966ce2139
Kidus Gebru <kidusgebruhagos@gmail.com>
2023-10-11 03:59:08 -0700

    Complete Task 2 of 226

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * int_index - Searches an array for an integer
+ * @array: Pointer to array of integers
+ * @size: Size of the pointer
+ * @cmp: Pointer to comparator function
+ *
+ * Return: Index of matched element or -1
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0, ret = -1;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+			{
+				ret = i;
+				break;
+			}
+		}
+	}
+	return (ret);
+
+}
