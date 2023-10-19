b9b7582139631eb88da00df9639e05a2279cf8c6
Kidus Gebru <kidusgebruhagos@gmail.com>
2023-10-19 15:52:39 -0700

    Iteration 1 of Task 5 of 229

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,7 +6,8 @@
  * Return: Always void
  */
 
-__attribute__((constructor)) void run_first(void)
+void __attribute__((constructor)) run_first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
 }
