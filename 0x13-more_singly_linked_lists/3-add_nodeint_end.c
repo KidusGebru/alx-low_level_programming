b8333d9e9d35a0f8330c1e0853f80cd258074381
Kidus Gebru <kidusgebruhagos@gmail.com>
2023-10-23 13:28:17 -0700

    Iteration 1 of Task 3 of 230

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -25,6 +25,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new = malloc(sizeof(listint_t)), *last;
 
+	if (head == NULL || new == NULL)
+		return (NULL);
 	new->next = NULL;
 	if (*head == NULL)
 		*head = new;
