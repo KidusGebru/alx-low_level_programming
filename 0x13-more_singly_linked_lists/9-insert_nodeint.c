4fa893ed773983e8e1a33110647e130acd295336
Kidus Gebru <kidusgebruhagos@gmail.com>
2023-10-24 12:27:48 -0700

    Iteration 2 of Task 230

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,17 +31,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	temp = *head;
-	while (temp->next)
+	while (temp)
 	{
-		i++;
-		temp = temp->next;
-
 		if (i == idx - 1)
 		{
 			new->next = temp->next;
 			temp->next = new;
 			return (new);
 		}
+		i++;
+		temp = temp->next;
+		
 	}
 	return (NULL);
 }
