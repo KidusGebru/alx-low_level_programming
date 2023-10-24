a09b09ec517569ee45d511133654765b39ad566a
Kidus Gebru <kidusgebruhagos@gmail.com>
2023-10-24 08:40:06 -0700

    Complete Task 5 of 230

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * * _free_list - Frees a listint_t list
+ * * @head: Pointer to start node
+ * *
+ * * Return: void
+ */
+void _free_list(listint_t *head)
+{
+	if (head->next)
+		_free_list(head->next);
+	free(head);
+}
+
+/**
+ * * free_listint2 - Frees a listint_t list and sets head to NULL
+ * * @head: Pointer to pointer of first node in the list
+ * *
+ * * Return: Void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (*head == NULL)
+	{
+		head = NULL;
+		return;
+	}
+
+	temp = *head;
+	if (temp->next)
+		_free_list(temp);
+
+	*head = NULL;
+
+}
