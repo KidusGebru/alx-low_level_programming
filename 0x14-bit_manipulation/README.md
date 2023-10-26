8adddbedbb969abf4b6375a11e16cd28a0e2592f
Kidus Gebru <kidusgebruhagos@gmail.com>
2023-10-26 04:02:39 -0700

    Complete Task 0 of 232

---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+This project contains solutions for tasks in 0x14. C - Bit manipulation - 230
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,8 @@
+#ifndef KID_MAIN_232
+#define KID_MAIN_232
+
+#include <stdlib.h>
+
+unsigned int binary_to_uint(const char *b);
+
+#endif
