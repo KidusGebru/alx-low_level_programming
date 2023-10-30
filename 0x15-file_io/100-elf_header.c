6dbe97c8dbb0fc0051ba19435f767f4edd73e114
Kidus Gebru <kidusgebruhagos@gmail.com>
2023-10-30 16:44:48 -0700

    Iteration 1 of Task 4 of 234

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -14,28 +14,28 @@ void _print_header(Elf64_Ehdr *header)
 {
 	int i = 0;
 
-	printf("Magic:	");
+	printf("Magic:   ");
 
 	for (i = 0; i < EI_NIDENT; i++)
 		printf("%02x ", header->e_ident[i]);
 	printf("\n");
 
-	printf("Class:				");
+	printf("Class:                             ");
 	printf("%s\n", header->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
-	printf("Data:				");
+	printf("Data:                              ");
 	if (header->e_ident[EI_DATA] == ELFDATA2LSB)
 		printf("2's complement, little endian\n");
 	else
 		printf("2's complement, big endian\n");
-	printf("Version:			");
+	printf("Version:                           ");
 	printf("%u\n", header->e_ident[EI_VERSION]);
-	printf("OS/ABI:				");
+	printf("OS/ABI:                            ");
 	printf("%u\n", header->e_ident[EI_OSABI]);
-	printf("ABI Version:			");
+	printf("ABI Version:                       ");
 	printf("%u\n", header->e_ident[EI_ABIVERSION]);
-	printf("Type:				");
+	printf("Type:                              ");
 	printf("%u\n", header->e_type);
-	printf("Entry point address:		");
+	printf("Entry point address:               ");
 	printf("%#010x\n", (unsigned int)header->e_entry);
 }
 
