0c43168ce93def46f2b8a9d6a5a61e943876842d
Kidus Gebru <kidusgebruhagos@gmail.com>
2023-10-30 16:55:26 -0700

    Iteration 1 of Task 4 of 234

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -14,28 +14,29 @@ void _print_header(Elf64_Ehdr *header)
 {
 	int i = 0;
 
-	printf("Magic:   ");
+	printf("ELF Header:\n");
+	printf("  Magic:   ");
 
 	for (i = 0; i < EI_NIDENT; i++)
 		printf("%02x ", header->e_ident[i]);
 	printf("\n");
 
-	printf("Class:                             ");
+	printf("  Class:                             ");
 	printf("%s\n", header->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
-	printf("Data:                              ");
+	printf("  Data:                              ");
 	if (header->e_ident[EI_DATA] == ELFDATA2LSB)
 		printf("2's complement, little endian\n");
 	else
 		printf("2's complement, big endian\n");
-	printf("Version:                           ");
+	printf("  Version:                           ");
 	printf("%u\n", header->e_ident[EI_VERSION]);
-	printf("OS/ABI:                            ");
+	printf("  OS/ABI:                            ");
 	printf("%u\n", header->e_ident[EI_OSABI]);
-	printf("ABI Version:                       ");
+	printf("  ABI Version:                       ");
 	printf("%u\n", header->e_ident[EI_ABIVERSION]);
-	printf("Type:                              ");
+	printf("  Type:                              ");
 	printf("%u\n", header->e_type);
-	printf("Entry point address:               ");
+	printf("  Entry point address:               ");
 	printf("%#010x\n", (unsigned int)header->e_entry);
 }
 
@@ -76,5 +77,10 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	_print_header(&header);
+	if (close(fd) == -1)
+	{
+		dprintf(2, "Error: Can't close file %s\n", argv[1]);
+		exit(98);
+	}
 	return (0);
 }
