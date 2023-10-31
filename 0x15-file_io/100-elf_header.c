c546bc554baf1f1e2a3a3a343f62c3da1410d33b
Kidus Gebru <kidusgebruhagos@gmail.com>
2023-10-31 11:34:44 -0700

    Iteration 2 of Task 4 of 234

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -5,6 +5,74 @@
 #include <elf.h>
 
 /**
+ * _get_osabi - Printss appropriate string for OS/ABI
+ * @val: OS/ABI number of ELF
+ *
+ * Return: Void
+ */
+void _get_osabi(unsigned int val)
+{
+	char *str = NULL;
+
+	switch (val)
+	{
+		case 0:
+			str = "UNIX - System V";
+			break;
+		case 2:
+			str = "UNIX - NetBSD";
+			break;
+		case 6:
+			str = "UNIX - Solaris";
+			break;
+		case 9:
+			str = "UNIX - FreeBSD";
+			break;
+		case 12:
+			str = "UNIX - OpenBSD";
+			break;
+	}
+	if (str == NULL)
+		printf("<unknown: %u>\n", val);
+	else
+		printf("%s\n", str);
+}
+
+/**
+ * _get_type - Prints appropriate string for Type of EFI file
+ * @val: Type number of ELF
+ *
+ * Return: Void
+ */
+void _get_type(unsigned int val)
+{
+	char *str = NULL;
+
+	switch (val)
+	{
+		case 0:
+			str = "NONE (No file type)";
+			break;
+		case 1:
+			str = "REL (Relocatable file)";
+			break;
+		case 2:
+			str = "EXEC (Executable file)";
+			break;
+		case 3:
+			str = "DYN (Shared object file)";
+			break;
+		case 4:
+			str = "CORE (Core file)";
+			break;
+	}
+	if (str == NULL)
+		printf("<unknown: %u>\n", val);
+	else
+		printf("%s\n", str);
+}
+
+/**
  * _print_header - Prints ELF header to stdout
  * @header: Struct header
  *
@@ -15,28 +83,31 @@ void _print_header(Elf64_Ehdr *header)
 	int i = 0;
 
 	printf("ELF Header:\n");
-	printf("	Magic:	");
+	printf("  Magic:   ");
 
 	for (i = 0; i < EI_NIDENT; i++)
 		printf("%02x ", header->e_ident[i]);
 	printf("\n");
 
-	printf("	Class:				");
+	printf("  Class:                             ");
 	printf("%s\n", header->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
-	printf("	Data:				");
+	printf("  Data:                              ");
 	if (header->e_ident[EI_DATA] == ELFDATA2LSB)
 		printf("2's complement, little endian\n");
 	else
 		printf("2's complement, big endian\n");
-	printf("	Version:			");
-	printf("%u\n", header->e_ident[EI_VERSION]);
-	printf("	OS/ABI:				");
-	printf("%u\n", header->e_ident[EI_OSABI]);
-	printf("	ABI Version:			");
+	printf("  Version:                           ");
+	if (header->e_ident[EI_VERSION] == 1)
+		printf("1 (current)\n");
+	else
+		printf("%u\n", header->e_ident[EI_VERSION]);
+	printf("  OS/ABI:                            ");
+	_get_osabi(header->e_ident[EI_OSABI]);
+	printf("  ABI Version:                       ");
 	printf("%u\n", header->e_ident[EI_ABIVERSION]);
-	printf("	Type:				");
-	printf("%u\n", header->e_type);
-	printf("	Entry point address:		");
+	printf("  Type:                              ");
+	_get_type(header->e_type);
+	printf("  Entry point address:               ");
 	printf("%#010x\n", (unsigned int)header->e_entry);
 }
 
