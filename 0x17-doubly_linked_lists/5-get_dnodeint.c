b75121cb95e453c0b42c0ea97e9baa944ed2a37c
Kidus Gebru <kidusgebruhagos@gmail.com>
2024-03-21 13:40:45 -0700

    Task 5.17

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - Return the nth node
+ * get_dnodeint_at_index - Return the nth node
  * @head: Pointer to first node of list
  * @index: 0 based index of desired node in the list
  *
  * Return: Pointer ot nth node
  */
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 
