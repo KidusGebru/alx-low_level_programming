bcaddb132803f72c56a90f713d2786618f5c3f02
Kidus Gebru <kidusgebruhagos@gmail.com>
2024-03-21 13:30:08 -0700

    Task 4.17

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,16 +1,16 @@
 #include "lists.h"
 
 /**
- * free_listint - Frees a listint_t list
+ * free_dlistint - Frees a dlistint_t list
  * @head: Pointer ot first node in a list
  *
  * Return: Void
  */
-void free_listint(listint_t *head)
+void free_dlistint(dlistint_t *head)
 {
 	if (head == NULL)
 		return;
 	if (head->next)
-		free_listint(head->next);
+		free_dlistint(head->next);
 	free(head);
 }
