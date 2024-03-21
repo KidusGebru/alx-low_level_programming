53cb2c745036452ab4ee6872d39f5d49ebcc612f
Kidus Gebru <kidusgebruhagos@gmail.com>
2024-03-21 13:47:23 -0700

    Task 6.17

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,17 +1,17 @@
 #include "lists.h"
 
 /**
- * sum_listint - Sums all the data in a listint_t linked list
+ * sum_dlistint - Sums all the data in a dlistint_t linked list
  * @head: Pointer to the first node
  *
  * Return: Sum of all the data
  */
-int sum_listint(listint_t *head)
+int sum_dlistint(dlistint_t *head)
 {
 	if (head != NULL)
 	{
 		if (head->next)
-			return (head->n + sum_listint(head->next));
+			return (head->n + sum_dlistint(head->next));
 		return (head->n);
 	}
 	return (0);
