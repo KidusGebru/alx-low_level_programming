6e70b58952dcb43b20f48c352a6a07a5dbf77606
Kidus Gebru <kidusgebruhagos@gmail.com>
2024-03-25 07:32:51 -0700

    Task 2.18

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,59 @@
+/**
+ * add - Adds two operands
+ * @a: Operand One
+ * @b: Operand two
+ *
+ * Return: Result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - Subtracts second operand from the first one
+ * @a: First operand
+ * @b: Second operand
+ *
+ * Return: Result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - Multiplies the two operands
+ * @a: Operand one
+ * @b: Operand two
+ *
+ * Return: Result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - Divides first operand by the second one
+ * @a: Dividen
+ * @b: Divider
+ *
+ * Return: Result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - Modulo operation
+ * @a: First operand
+ * @b: Second operand
+ *
+ * Return: Result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
